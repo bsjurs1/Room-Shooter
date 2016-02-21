0f7e38cf2f35c2456cd652c3337271ed4fc5ce31
mohff <vegard.knuts@gmail.com>
2016-02-21 12:54:10 +0100

    add git ignore files

---
+++ mac-client/Room Shooter/Network.swift
@@ -0,0 +1,15 @@
+//
+//  Network.swift
+//  Room Shooter
+//
+//  Created by Bjarte Sjursen on 19.02.2016.
+//  Copyright © 2016 Sjursen Software. All rights reserved.
+//
+
+import Foundation
+
+class Network {
+    
+    
+    
+}
